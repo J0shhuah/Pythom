77b678b47f97851ce7640c39f5de0749609614d8
unknown <senai@NSN119107.edu119.local>
2023-09-28 16:49:13 -0300

    Comentário

---
+++ List.py
@@ -0,0 +1,7 @@
+a = ['Vermelha', 'Amarelo', 'Azul']
+b = a.copy()
+b.append(a)
+print(a)
+print(b)
+b.remove('Verde')
+
--- Tuplas.py
+++ Tuplas.py
@@ -1,8 +1,14 @@
-tuplaValor = (10,2,3,4,1,15)
-print (type(tuplaValor))
-print(tuplaValor[-6])
-print(len(tuplaValor))
-for i in range(0,len(tuplaValor)):
-    print(tuplaValor[i])
-r = tuplaValor[i]*2
-print(r)
+vetA=(2,5,6,4,2,3)
+vetB=(3,4,5,6,8,2)
+for valor in range(0,len(vetA)):
+    r = vetA[valor]*vetB[valor]
+    print(f'{vetA[valor]} * {vetB[valor]} = {r}')
+
+x = int(input('Digite o número: '))
+g = [1,3]
+g.append(x)
+print(g)
+
+g.remove(x)
+print(g)
+
